99e0a6a5c79ebbca8586e72935af56fe26230197
marco <mpao@me.com>
2018-03-02 10:55:40 +0100

    set up UI

--- app/src/main/java/io/github/mpao/fcmexperiments/MainActivity.java
+++ app/src/main/java/io/github/mpao/fcmexperiments/MainActivity.java
@@ -2,12 +2,31 @@ package io.github.mpao.fcmexperiments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isEnabled;
+    private TextView type, message, params;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SwitchCompat enable = findViewById(R.id.enable);
+        type    = findViewById(R.id.type);
+        message = findViewById(R.id.message);
+        params  = findViewById(R.id.params);
+        enable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isEnabled = isChecked;
+            }
+        });
+
     }
+
 }
